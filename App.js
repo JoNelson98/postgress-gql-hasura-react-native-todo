7d19079e750db0bf666baebb532e37a7d2d0959a
JoNelson98 <jayneltech@gmail.com>
2022-05-11 10:48:21 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Main } from "./.expo/src/Main";
+import { Auth } from "./.expo/src/Auth";
 
 export default function App() {
+  const [isLogggedIn, setIsLoggedIn] = useState(false);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {isLogggedIn ? <Main /> : <Auth onLogin={() => setIsLoggedIn(true)} />}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ config.js
@@ -0,0 +1,6 @@
+const AUTH_CLIENT_ID = "pVWF5815fDO80L0MVihTJGHTEpx0RGe0";
+const AUTH_DOMAIN = "http://dev-azms87cd.us.auth0.com";
+const ID_TOKEN_KEY = "id_token";
+const NONCE_KEY = "notice";
+
+export { AUTH_CLIENT_ID, AUTH_DOMAIN, ID_TOKEN_KEY, NONCE_KEY };
