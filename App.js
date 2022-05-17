7f8f7a84e94725363d97dedd956e95e5fead84c4
JoNelson98 <jayneltech@gmail.com>
2022-05-17 12:29:08 -0500

    adding auth0 and gql queries

--- App.js
+++ App.js
@@ -3,12 +3,40 @@ import { StyleSheet, Text, View } from "react-native";
 import React, { useState } from "react";
 import { Main } from "./.expo/src/Main";
 import { Auth } from "./.expo/src/Auth";
+import * as SecureStore from "expo-secure-store";
+import { ID_TOKEN_KEY } from "./config";
 
 export default function App() {
-  const [isLogggedIn, setIsLoggedIn] = useState(false);
+  const [authToken, setToken] = useState(null);
+  const [user, setNewUser] = useState(null);
+
+  const onLogin = async (isNewUser = false) => {
+    console.log("onlogin fired!!");
+    await SecureStore.getItemAsync(ID_TOKEN_KEY).then((session) => {
+      if (session) {
+        const sessionObj = JSON.parse(session);
+        const { exp, token, id, name } = sessionObj;
+        if (exp > Date.now() / 1000) {
+          setToken(token);
+          setNewUser({ id, name, isNewUser });
+        }
+      }
+    });
+  };
+
+  React.useEffect(() => {
+    console.log("authToken: ", authToken);
+    onLogin();
+  }, []);
+
   return (
     <View style={styles.container}>
-      {isLogggedIn ? <Main /> : <Auth onLogin={() => setIsLoggedIn(true)} />}
+      {authToken && user && <Main authToken={authToken} user={user} />}
+      <Auth
+        authToken={authToken}
+        onLogout={() => setToken(null)}
+        onLogin={onLogin}
+      />
       <StatusBar style="auto" />
     </View>
   );
--- config.js
+++ config.js
@@ -1,6 +1,15 @@
 const AUTH_CLIENT_ID = "pVWF5815fDO80L0MVihTJGHTEpx0RGe0";
 const AUTH_DOMAIN = "http://dev-azms87cd.us.auth0.com";
-const ID_TOKEN_KEY = "id_token";
-const NONCE_KEY = "notice";
+const ID_TOKEN_KEY = "todo-id-token";
+const NONCE_KEY = "todo-nonce";
+const GRAPHQL_ENDPOINT = "https://secure-koi-90.hasura.app/v1/graphql";
+const AUTH_NAMESPACE = "https://user";
 
-export { AUTH_CLIENT_ID, AUTH_DOMAIN, ID_TOKEN_KEY, NONCE_KEY };
+export {
+  AUTH_CLIENT_ID,
+  AUTH_DOMAIN,
+  ID_TOKEN_KEY,
+  NONCE_KEY,
+  GRAPHQL_ENDPOINT,
+  AUTH_NAMESPACE,
+};
